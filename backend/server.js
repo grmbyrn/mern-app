6247b79d780fbb021f990b99a7e9b07b6a10d6d6
grmbyrn <graemebyrne_89@hotmail.com>
2022-09-27 23:25:54 +0200

    added route and controllers folders with files for goalRoutes and goalController

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.port || 5000
+
+const app = express()
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => console.log(`Port started on port ${port}`))
